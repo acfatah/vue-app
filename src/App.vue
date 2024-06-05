4029b7c91b939e55e416cb89b89bc7ac4696c388
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-06-05 15:17:23 +0800

    Add @storybook/addon-themes

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,24 @@ import BlankLayout from './layouts/BlankLayout.vue'
 const route = useRoute()
 const theme = useStorage('theme', 'light')
 
-// Use the followig value to detect default user preference scheme
-// eslint-disable-next-line unused-imports/no-unused-vars
-const preferColorSchemeIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+function setTheme() {
+  // Use the followig value to detect default user preference scheme
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const preferColorSchemeIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches
 
-onMounted(() => {
   if (theme.value === 'dark')
     document.documentElement.classList.add('dark')
   else
     document.documentElement.classList.remove('dark')
+}
+
+function setBackgroundColor() {
+  document.getElementById('app')?.classList.add('dark:bg-neutral-950')
+}
+
+onMounted(() => {
+  setBackgroundColor()
+  setTheme()
 })
 </script>
 
