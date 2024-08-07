a57b96225cb2269aa8c10637973ac986187646a1
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-08-08 07:45:15 +0800

    Add unpluggin-vue-components package
    On-demand components auto importing.

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader({
-    svgoConfig: {
-      multipass: true,
-      plugins: [
-        {
-          name: 'preset-default',
-          params: {
-            overrides: {
-              // viewBox is required to resize SVGs with CSS.
-              // @see https://github.com/svg/svgo/issues/1128
-              removeViewBox: false,
+  plugins: [
+    Components({
+      dts: false,
+    }),
+    vue(),
+    svgLoader({
+      svgoConfig: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                // viewBox is required to resize SVGs with CSS.
+                // @see https://github.com/svg/svgo/issues/1128
+                removeViewBox: false,
+              },
             },
           },
-        },
-      ],
-    },
-  })],
+        ],
+      },
+    }),
+  ],
 })
