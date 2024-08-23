1a219f477183503cd4d064ceefd3a5cbe70d888d
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-08-23 15:45:11 +0800

    Update Placeholder story

--- stories/components/Placeholder.stories.js
+++ stories/components/Placeholder.stories.js
@@ -2,21 +2,39 @@ import '~/styles/app.css'
 import Placeholder from '~/components/Placeholder.vue'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+
+/**
+ * Placeholder component is used to display a temporary visual block that mimics
+ * the shape and size of the content it represents.
+ */
 export default {
   title: 'Components/Placeholder',
   component: Placeholder,
   tags: ['autodocs'],
 }
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Placeholder />',
+      },
+    },
+  },
   render: () => ({
     components: { Placeholder },
     template: '<Placeholder />',
   }),
 }
 
-export const WithDefaultSlot = {
+export const WithContent = {
+  parameters: {
+    docs: {
+      source: {
+        code: '<Placeholder>Using default slot</Placeholder>',
+      },
+    },
+  },
   render: () => ({
     components: { Placeholder },
     template: '<Placeholder>Using default slot</Placeholder>',
