c92f9283c8c203fe706045a63ad2f2c67a5201ba
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-08-29 22:19:03 +0800

    Update use neutral color

--- stories/layout/Toolbar.stories.js
+++ stories/layout/Toolbar.stories.js
@@ -4,26 +4,26 @@ import '~/styles/app.css'
 import Placeholder from '~/components/Placeholder.vue'
 
 const toolbarTemplate = `
-<header class="bg-background flex h-9 items-center px-2 space-x-1 rounded-none border-b border-gray-200 shadow-sm lg:px-4">
+<header class="bg-background flex h-9 items-center px-2 space-x-1 rounded-none border-b border-neutral-200 shadow-sm lg:px-4">
   <div class="flex w-full items-center">
     <h2 class="p-2 text-sm font-semibold">Vue App</h2>
     <!-- desktop display -->
     <div class="hidden justify-between gap-2 md:flex md:grow md:items-center">
       <div class="flex">
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           File
         </button>
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           Edit
         </button>
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           View
         </button>
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           Account
         </button>
       </div>
-      <button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <button type="button" class="ring-neutral-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-xicon size-4"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg><span class="sr-only">Close</span>
       </button>
     </div>
@@ -31,7 +31,7 @@ const toolbarTemplate = `
   <div>
     <!-- mobile display -->
     <div class="flex md:hidden">
-      <button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <button type="button" class="ring-neutral-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical-icon size-4"><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle cx="12" cy="19" r="1" /></svg><span class="sr-only">More</span>
       </button>
     </div>
@@ -87,27 +87,27 @@ export const Default = {
 }
 
 const toolbarThreeColumnsTemplate = `
-<header class="bg-background flex h-9 items-center px-2 space-x-1 rounded-none border-b border-gray-200 shadow-sm lg:px-4">
+<header class="bg-background flex h-9 items-center px-2 space-x-1 rounded-none border-b border-neutral-200 shadow-sm lg:px-4">
   <div class="flex w-full items-center">
     <h2 class="p-2 text-sm font-semibold md:hidden">Vue App</h2>
     <!-- desktop display -->
     <div class="hidden justify-between gap-2 md:flex md:grow md:items-center">
       <div class="flex">
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           File
         </button>
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           Edit
         </button>
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           View
         </button>
-        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
           Account
         </button>
       </div>
       <h2 class="absolute left-1/2 transform -translate-x-1/2 p-2 text-sm font-semibold">Vue App</h2>
-      <button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <button type="button" class="ring-neutral-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-xicon size-4"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg><span class="sr-only">Close</span>
       </button>
     </div>
@@ -115,7 +115,7 @@ const toolbarThreeColumnsTemplate = `
   <div>
     <!-- mobile display -->
     <div class="flex w-full items-center md:hidden">
-      <button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+      <button type="button" class="ring-neutral-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical-icon size-4"><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle cx="12" cy="19" r="1" /></svg><span class="sr-only">More</span>
       </button>
     </div>
@@ -154,7 +154,7 @@ export const ToolbarWithThreeColumns = {
 }
 
 const toolbarButtonTemplate = `
-<button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+<button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-neutral-100">
   File
 </button>
 `
@@ -177,7 +177,7 @@ export const ToolbarButton = {
 }
 
 const toolbarMenuIconButtonTemplate = `
-<button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+<button type="button" class="ring-neutral-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical-icon size-4"><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle cx="12" cy="19" r="1" /></svg><span class="sr-only">More</span>
 </button>
 `
@@ -197,7 +197,7 @@ export const ToolbarMenuIconButton = {
 }
 
 const toolbarCloseButtonTemplate = `
-<button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+<button type="button" class="ring-neutral-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-xicon size-4"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg><span class="sr-only">Close</span>
 </button>
 `
