e9762d957a254681c3616e53ced6734086a4244b
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-08-29 17:18:59 +0800

    Add three columns toolbar

--- stories/layout/Toolbar.stories.js
+++ stories/layout/Toolbar.stories.js
@@ -62,13 +62,15 @@ export default {
 export const Default = {
   parameters: {
     layout: 'fullscreen',
+
     docs: {
       source: {
         code: toolbarTemplate,
       },
+
       story: {
         inline: false,
-        height: '668px',
+        height: '108px',
       },
     },
   },
@@ -81,6 +83,73 @@ export const Default = {
     },
 
     template: toolbarTemplate,
+  }),
+}
+
+const toolbarThreeColumnsTemplate = `
+<header class="bg-background flex h-9 items-center px-2 space-x-1 rounded-none border-b border-gray-200 shadow-sm lg:px-4">
+  <div class="flex w-full items-center">
+    <h2 class="p-2 text-sm font-semibold md:hidden">Vue App</h2>
+    <!-- desktop display -->
+    <div class="hidden justify-between gap-2 md:flex md:grow md:items-center">
+      <div class="flex">
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+          File
+        </button>
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+          Edit
+        </button>
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+          View
+        </button>
+        <button class="relative flex cursor-default select-none items-center rounded-sm px-3 py-1 text-sm font-medium outline-none focus:bg-gray-100">
+          Account
+        </button>
+      </div>
+      <h2 class="absolute left-1/2 transform -translate-x-1/2 p-2 text-sm font-semibold">Vue App</h2>
+      <button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-xicon size-4"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg><span class="sr-only">Close</span>
+      </button>
+    </div>
+  </div>
+  <div>
+    <!-- mobile display -->
+    <div class="flex w-full items-center md:hidden">
+      <button type="button" class="ring-gray-300 focus:ring-ring rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical-icon size-4"><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle cx="12" cy="19" r="1" /></svg><span class="sr-only">More</span>
+      </button>
+    </div>
+  </div>
+</header>
+`
+
+/**
+ * Toolbar is a grouping component for buttons and other content.
+ */
+export const ToolbarWithThreeColumns = {
+  parameters: {
+    layout: 'fullscreen',
+
+    docs: {
+      source: {
+        code: toolbarThreeColumnsTemplate,
+      },
+
+      story: {
+        inline: false,
+        height: '108px',
+      },
+    },
+  },
+
+  render: args => ({
+    components: { Placeholder },
+
+    setup() {
+      return { args }
+    },
+
+    template: toolbarThreeColumnsTemplate,
   }),
 }
 
